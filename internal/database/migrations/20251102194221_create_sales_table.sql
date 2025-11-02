400c7af51866d8eaa879d500d985eb9a42368be0
PC-ZUBU <igorlima0748@gmail.com>
2025-11-02 18:45:51 -0300

    Update

--- internal/database/migrations/20251102194221_create_sales_table.sql
+++ internal/database/migrations/20251102194221_create_sales_table.sql
@@ -2,6 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS sales (
     id BIGSERIAL PRIMARY KEY,
+    sale_value NUMERIC(18,2) NOT NULL DEFAULT 0,
     sale_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 -- +goose StatementEnd
